762bca16157c4a40f23d1817ffc19e653a20e0ca
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2024-02-18 13:08:16 +0600

    redux setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
 
 function App() {
 
   return (
-    <>
-     <h1 className='text-5xl'>Hello, Developer</h1>
-    </>
+    <Provider store={store}>
+     <h1 className='text-5xl font-bold'>Hello, Developer</h1>
+    </Provider>
   )
 }
 
